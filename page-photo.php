65c509d6a9018a1430e706aebe5ecb56834429f4
Khen Solomon Lethil <khensolomon@gmail.com>
2016-09-14 22:31:34 +0200

    category_nicename

--- page-photo.php
+++ page-photo.php
@@ -18,7 +18,20 @@
 			$category_link_name = get_query_var('gallery');
 			// set_query_var('cat_link_id', $category_link_id);
 			// $category_link_name = get_query_var( 'gallery')
-			if ($category_link_name && $catId = array_search($category_link_name, array_column($categories, 'category_nicename'))) {
+			// $abc = array_keys(array_column($categories, 'category_nicename'), $category_link_name);
+			// $abc = array_search(array('category_nicename' => $category_link_name), $categories);
+			function searchArrayKeyVal($sKey, $id, $sArray) {
+			   foreach ($sArray as $key => $val) {
+			       if ($val->{$sKey} == $id) {
+			           return $val;
+			       }
+			   }
+			   return false;
+			}
+			$catId = searchArrayKeyVal('category_nicename',$category_link_name,$categories);
+			// print_r($catId);
+			// $catId = array_search($category_link_name, array_column($categories, 'category_nicename'));
+			if ($category_link_name && $catId) {
 				
 				// $catObject = array_filter( $categories, function ($e) use($category_link_name) { return $e->category_nicename == $category_link_name; });
 				// $catId = array_search($category_link_name, array_column($categories, 'category_nicename'),false)
@@ -26,7 +39,8 @@
 				// print_r(array_shift(array_slice($catObject, 0, 1)));
 				// print_r(array_shift(array_slice($categories, $catId, 1)));
 
-				set_query_var('catGallery', array_shift(array_slice($categories, $catId, 1)));
+				// set_query_var('catGallery', array_shift(array_slice($categories, $catId, 1)));
+				set_query_var('catGallery', $catId);
 				get_template_part(
 					'template/photo',
 					'album'
